051007b74bb16a605fe5cb882f9be9a2d381024f
salman729 <sk9623854@gmail.com>
2021-05-03 23:39:26 +0500

    today532021latestone

--- resources/views/front-end-layout/app.blade.php
+++ resources/views/front-end-layout/app.blade.php
@@ -83,13 +83,15 @@
 });
         </script>
         <script type="text/javascript">
-  function myInputcopyLik() {
-  var copyText = document.getElementById("myInputcopyLik");
-  copyText.select();
-  copyText.setSelectionRange(0, 99999)
-  document.execCommand("copy");
-  toastr.success('Profile Link Copy Successfully...!');
+ 
 
+function copyToClipboard(element) {
+ var $temp = $("<input>");
+ $("body").append($temp);
+ $temp.val($(element).html()).select();
+ document.execCommand("copy");
+ $temp.remove();
+  toastr.success('Profile Link Copy Successfully...!');
 }
 
 
--- resources/views/frontEnd/myAccount/copyinvitation.blade.php
+++ resources/views/frontEnd/myAccount/copyinvitation.blade.php
@@ -4,9 +4,12 @@
 <h3 class="CopyLink_copyLinkTitle__3Yykr">Your Link:</h3>
 <h3 class="CopyLink_accountUrl__2ZKBH">{{ route('front-home') }}/{{ $userLinker->username }}</h3>
 <div class="CopyLink_buttonContainer__2woX2">
-<input type="text" value="{{ route('front-home') }}/{{ $userLinker->username }}" id="myInputcopyLik" style="display: none;">
-<div class="CopyLink_copyLinkButton__HAu09" onclick="myInputcopyLik()">
+<p id="texttocopy" style="display: none;"> 
+{{ route('front-home') }}/{{ $userLinker->username }}
+</p>
 
+<div class="CopyLink_copyLinkButton__HAu09" onclick="copyToClipboard('#texttocopy')">
+  
 <img
 src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABIAAAASCAYAAABWzo5XAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAD9SURBVHgBrZIxEoIwFET3RwpKKO2wo/UEcgM9Qm7kEYw30BvgCbSkpLNxxnQ6OviNiqJOAuKwDTBLNpv3Q3AokvvARzFBo3pppsKcbFYs95EArxkcoFn6ADHw7F6RMChgQIFE6ozgS2KaSJ+KsYc6EafZLJy77FjuTA7J27tAR7o3+gYrQCNGO3kVWHqBbRtSNrKALSGihSrYb2DfIf6qbmE7d2FKHu1cLaqhWIN89BYnXKbmJ0k1R7yFmF0083llDdqoUMdyOwT5o2rVYwDft/3AepmrgXYeLVP93Dzy5/drAI7b3j3sNmCtQf+Atfn3BgZs9AHWoWMJ1uZdAYLmcIwEdyPUAAAAAElFTkSuQmCC"
 alt=""
